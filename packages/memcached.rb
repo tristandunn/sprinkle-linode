1019a9988f691bd8f7c917c9ea1aaa08ba0e4332
Tristan Dunn <tristanzdunn@gmail.com>
2009-02-04 17:53:43 -0600

    Add a line break.

--- packages/memcached.rb
+++ packages/memcached.rb
@@ -1,6 +1,7 @@
 package :memcached do
   describe 'memcached distributed memory object caching system'
   apt 'memcached'
+
   verify do
     has_process    'memcached'
     has_executable '/usr/bin/memcached'
