f49b123a9089bfc40c3cb46dc789f527e3b97af5
Tristan Dunn <tristanzdunn@gmail.com>
2010-09-12 13:00:47 -0400

    Update git, Passenger, Ruby, and Rubygems.

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -1,20 +1,24 @@
 package :ruby do
   description 'Ruby Virtual Machine'
-  version '1.8.6'
-  patchlevel '383'
+  version '1.8.7'
+  patchlevel '302'
   source "ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-#{version}-p#{patchlevel}.tar.gz"
   requires :ruby_dependencies
+
+  verify do
+    has_executable 'ruby'
+  end
 end
 
 package :ruby_dependencies do
   description 'Ruby Virtual Machine Build Dependencies'
-  apt %w( bison zlib1g-dev libssl-dev libreadline5-dev libncurses5-dev file )
+  apt %w(bison zlib1g-dev libssl-dev libreadline5-dev libncurses5-dev file)
 end
 
 package :rubygems do
   description 'Ruby Gems Package Management System'
-  version '1.3.5'
-  source "http://rubyforge.org/frs/download.php/60718/rubygems-#{version}.tgz" do
+  version '1.3.7'
+  source "http://production.cf.rubygems.org/rubygems/rubygems-#{version}.tgz" do
     custom_install 'ruby setup.rb'
   end
   requires :ruby
--- packages/scm.rb
+++ packages/scm.rb
@@ -1,6 +1,6 @@
 package :git, :provides => :scm do
   description 'Git version control client'
-  version '1.6.4'
+  version '1.7.2'
   source "http://kernel.org/pub/software/scm/git/git-#{version}.tar.gz"
   requires :git_dependencies
 end
