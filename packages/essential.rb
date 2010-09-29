14f23bd030e214bb92cf901b81703da4b9e3dd23
Tristan Dunn <tristanzdunn@gmail.com>
2010-09-29 17:59:45 -0400

    Remove custom source.

--- packages/essential.rb
+++ packages/essential.rb
@@ -1,7 +1,6 @@
 package :build_essential do
   describe 'Build tools'
   apt 'build-essential zlib1g-dev libssl-dev libreadline5-dev' do
-    pre :install, "echo 'deb http://ftp.debian.org/debian experimental main' >> /etc/apt/sources.list"
     pre :install, 'apt-get -y update',
                   'apt-get -y dist-upgrade'
   end
